49494224575bc9a8cbb940792df8821c7b546591
Sean Mosely <sean.mosely@gmail.com>
2016-10-04 16:12:34 +1300

    fixed setup.py script

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-	'name':'PyMets',
+	'name':'mets_dnx',
 	'version':'0.1.0',
 	'author':'Sean Mosely',
 	'author_email':'sean.mosely@gmail.com',
-	'packages':['pymets','pymets.tests'],
-	'description':'Python library for building METS XML documents',
-	'install_requires':['lxml', 'nose']}
+	'packages':['mets_dnx',],
+	'description':'Python library for building Rosetta DNX/METS XML documents',
+	'install_requires':['lxml', 'nose', 'pymets','pydc', 'pydnx']}
 
 setup(**config)
