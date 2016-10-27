b1f5354b6725f6c34ba374ef7d76bf8b8f7ce791
Sean Mosely <sean.mosely@gmail.com>
2016-10-27 14:34:59 +1300

    Added build_sip_from_json function

--- mets_dnx/factory.py
+++ mets_dnx/factory.py
@@ -1,4 +1,5 @@
 import hashlib
+import json
 import os
 import re
 import time
@@ -7,7 +8,7 @@ from lxml import etree as ET
 
 from pydc import factory as dc_factory
 from pymets import mets_factory as mf
-from pymets import mets_model
+from pymets import mets_model as mm
 from pydnx import factory as dnx_factory
 
 def generate_md5(filepath, block_size=2**20):
@@ -339,30 +340,306 @@ def build_single_file_mets(ie_dmd_dict=None,
 
     # build filesec
     filename = os.path.basename(filepath)
-    filesec = mets_model.FileSec()
-    filegrp = mets_model.FileGrp(ID="rep1", ADMID="rep1-amd", USE="VIEW")
+    filesec = mm.FileSec()
+    filegrp = mm.FileGrp(ID="rep1", ADMID="rep1-amd", USE="VIEW")
     filesec.append(filegrp)
 
-    file_el = mets_model.File(ID='fid1-1', ADMID="fid1-1-amd")
+    file_el = mm.File(ID='fid1-1', ADMID="fid1-1-amd")
     filegrp.append(file_el)
 
-    flocat = mets_model.FLocat(LOCTYPE="URL", href=filename)
+    flocat = mm.FLocat(LOCTYPE="URL", href=filename)
     file_el.append(flocat)
 
     mets.append(filesec)
     
     # build structmap
-    structmap = mets_model.StructMap(ID="rep1-1", TYPE="PHYSICAL")
+    structmap = mm.StructMap(ID="rep1-1", TYPE="PHYSICAL")
 
-    div_1 = mets_model.Div(LABEL="Preservation Master")
+    div_1 = mm.Div(LABEL="Preservation Master")
     structmap.append(div_1)
 
-    div_2 = mets_model.Div(TYPE="FILE", LABEL=filename)
+    div_2 = mm.Div(TYPE="FILE", LABEL=filename)
     div_1.append(div_2)
 
-    fptr = mets_model.Fptr(FILEID='fid1-1')
+    fptr = mm.Fptr(FILEID='fid1-1')
     div_2.append(fptr)
 
     mets.append(structmap)
+
+    return mets
+
+def _build_fl_amd_from_json(mets, file_no, rep_no, item, path):
+    fl_amd_sec = mm.AmdSec(ID="fid{}-{}".format(file_no, rep_no))
+    fileOriginalName = None
+    fileSizeBytes = None
+    md5sum = None
+    fileCreationDate = None
+    fileModificationDate = None
+    note = None
+    # events = None
+
+    gfc = {} # general file characteristics
+    fixity = {}
+    events = {}
+    if path != None:
+        gfc['fileOriginalPath'] = os.path.join(path, item["name"])
+    else:
+        gfc['fileOriginalPath'] = item["name"]
+    for key in item.keys():
+        if key == 'name':
+            gfc['fileOriginalName'] = item[key]
+        if key == 'fileSizeBytes':
+            gfc['fileSizeBytes'] = item[key]
+        if key == 'fileCreationDate':
+            gfc['fileCreationDate'] = item[key]
+        if key == 'fileModificationDate':
+            gfc['fileModificationDate'] = item[key]
+        if key == 'note':
+            gfc['note'] = item[key]
+
+        # fixity values
+        if key.upper() == 'MD5':
+            fixity['fixityType'] = 'MD5'
+            fixity['fixityValue'] = item[key]
+        # provenance values
+        if key == 'events':
+            events = item[key]
+
+    # Check the supplied fixity to make sure it can be reproduced
+    fileSizeBytes = None
+    if md5sum and fileOriginalpath:
+        fileSizeBytes = os.path.getsize(fileOriginalpath)
+        checksum = generate_md5(fileOriginalpath)
+        if md5sum != checksum:
+            raise ValueError("could not reproduce MD5 sum for {}: Expected {}, got {}".format(
+                fileOriginalPath, md5sum, checksum))
+
+    # reset any empty dicts to None value
+    if len(gfc) == 0:
+        gfc = None
+    if len(fixity) == 0:
+        fixity = None
+    if len(events) == 0:
+        events = None
+
+    fl_amd_tech = dnx_factory.build_file_amdTech(
+        generalFileCharacteristics=[gfc],
+        fileFixity=[fixity])
+    fl_amd_digiprov = dnx_factory.build_ie_amdDigiprov(
+        event=events) # yes, the call to ie amdDigiprov is intentional,
+    # as we don't yet have a file digiprov builder. Should be fine though.
+
+    build_amdsec(fl_amd_sec, tech_sec=fl_amd_tech, digiprov_sec=fl_amd_digiprov)
+    mets.append(fl_amd_sec)
+
+
+    fileSizeBytes = None
+    if md5sum and fileOriginalpath:
+        fileSizeBytes = os.path.getsize(fileOriginalpath)
+        checksum = generate_md5(fileOriginalpath)
+        if md5sum != checksum:
+            raise ValueError("could not reproduce MD5 sum for {}: Expected {}, got {}".format(
+                fileOriginalPath, md5sum, checksum))
+
+
+def _parse_json(mets, rep_no, json_doc, path=None):
+    if type(json_doc) == str:
+        rep_dict = json.loads(json_doc)
+    else:
+        rep_dict = json_doc
+    file_no = 1
+    for item in rep_dict:
+        try:
+            if item['type'] == 'file':
+                _build_fl_amd_from_json(mets, file_no, rep_no, item, path)
+                # Add file number to assist with filesec construction
+                item['file_no'] = file_no
+                file_no += 1
+            elif item['type'] == 'directory':
+                if path == None:
+                    path = os.path.join(item['name'])
+                else:
+                    path = os.path.join(path, item['name'])
+                _parse_json(mets, rep_no, item['children'], path)
+            else:
+                raise ValueError("This item does not have a valid type value: {}".format(item))
+        except KeyError:
+            print("The following json does not have a 'type' value: {}".format(item))
+
+
+def _parse_json_for_filegrp(filegrp, rep_no, json_doc, input_dir, path=None):
+    if type(json_doc) == str:
+        rep_dict = json.loads(json_doc)
+    else:
+        rep_dict = json_doc
+    file_no = 1
+    for item in rep_dict:
+        try:
+            if item['type'] == 'file':
+                # 2016-10-27: Hacky way of getting relative file location
+                # Simply shaving the input_dir off the front of the full path 
+                href = os.path.join(path, item['name'])[len(input_dir) + 1:]
+                file_el = mm.File(ID="fid{}-{}".format(file_no, rep_no))
+                flocat = mm.FLocat(LOCTYPE="URL", href=href)
+                file_el.append(flocat)
+                filegrp.append(file_el)
+                file_no += 1
+            elif item['type'] == 'directory':
+                if path == None:
+                    path = os.path.join(item['name'])
+                else:
+                    path = os.path.join(path, item['name'])
+                _parse_json_for_filegrp(filegrp, rep_no, item['children'], input_dir, path)
+            else:
+                raise ValueError("This item does not have a valid type value: {}".format(item))
+        except KeyError:
+            print("The following json does not have a 'type' value: {}".format(item))
+
+
+def _parse_json_for_structmap(div, rep_no, json_doc, path=None):
+    if type(json_doc) == str:
+        rep_dict = json.loads(json_doc)
+    else:
+        rep_dict = json_doc
+    file_no = 1
+    for item in rep_dict:
+        try:
+            if item['type'] == 'file':
+                # _build_fl_amd_from_json(mets, file_no, rep_no, item, path)
+                if 'label' in item.keys():
+                    label = item['label']
+                else:
+                    label = item['name']
+                div2 = mm.Div(TYPE="FILE", LABEL=label)
+                div.append(div2)
+
+                fptr = mm.Fptr(FILEID="fid{}-{}".format(file_no, rep_no))
+                div2.append(fptr)
+                
+                file_no += 1
+            elif item['type'] == 'directory':
+                if path == None:
+                    path = os.path.join(item['name'])
+                    div2 = mm.Div(TYPE="FOLDER", LABEL=item['name'])
+                else:
+                    path = os.path.join(path, item['name'])
+                    div2 = mm.Div(TYPE="FOLDER", LABEL=item['name'])
+                div.append(div2)
+                _parse_json_for_structmap(div2, rep_no, item['children'], path)
+            else:
+                raise ValueError("This item does not have a valid type value: {}".format(item))
+        except KeyError:
+            print("The following json does not have a 'type' value: {}".format(item))
+
+
+def _build_rep_amdsec(mets, rep_no, digital_original, preservation_type):
+    rep_amdsec = ET.Element("{http://www.loc.gov/METS/}amdSec", ID="rep{}-amd".format(rep_no))
+    general_rep_characteristics = [{'RevisionNumber': '1', 
+            'DigitalOriginal': str(digital_original).lower(),
+            'usageType': 'VIEW',
+            'preservationType': preservation_type}]
+    rep_amd_tech = dnx_factory.build_rep_amdTech(
+        generalRepCharacteristics=general_rep_characteristics)
+    rep_amd_rights = None
+    rep_amd_digiprov = None
+    rep_amd_source = None
+    build_amdsec(
+        rep_amdsec,
+        tech_sec=rep_amd_tech, 
+        rights_sec=rep_amd_rights,
+        source_sec=rep_amd_source, 
+        digiprov_sec=rep_amd_digiprov)
+    mets.append(rep_amdsec)
+
+def build_sip_from_json(ie_dmd_dict=None,
+                pres_master_json=None, 
+                modified_master_json=None,
+                access_derivative_json=None,
+                cms=None,
+                webHarvesting=None,
+                generalIECharacteristics=None,
+                objectIdentifier=None,
+                accessRightsPolicy=None,
+                eventList=None,
+                input_dir=None,
+                digital_original=False):
+    """Build a METS XML file using JSON-formatted data describing the 
+    rep structures, rather than directory paths."""
+    mets = mf.build_mets()
+    _build_ie_dmd_amd(mets,
+        ie_dmd_dict=ie_dmd_dict,
+        generalIECharacteristics=generalIECharacteristics,
+        cms=cms,
+        webHarvesting=webHarvesting,
+        objectIdentifier=objectIdentifier,
+        accessRightsPolicy=accessRightsPolicy,
+        eventList=eventList)
+
+    rep_no = 1
+    if pres_master_json != None:
+        pmd = json.loads(pres_master_json)
+        pm_rep_no = rep_no
+        # Build rep AMD Sec
+        _build_rep_amdsec(mets, rep_no, digital_original, 'PRESERVATION_MASTER')
+        # run through json structure and find files, assembling their
+        # filepaths along the way
+        _parse_json(mets, rep_no, pres_master_json)
+        rep_no += 1
+    if modified_master_json != None:
+        mm_rep_no = rep_no
+        mmd = json.loads(modified_master_json)
+        _build_rep_amdsec(mets, rep_no, digital_original, 'MODIFIED_MASTER')
+        # _build_fl_amd_from_json(mets, rep_no, modified_master_json)
+        _parse_json(mets, rep_no, modified_master_json)
+        rep_no += 1
+    if access_derivative_json != None:
+        add = json.loads(access_derivative_json)
+        ad_rep_no = rep_no
+        _build_rep_amdsec(mets, rep_no, digital_original, 'ACCESS_DERIVATIVE')
+        # _build_fl_amd_from_json(mets, rep_no, access_derivative_json)
+        _parse_json(mets, rep_no, access_derivative_json)
+        rep_no += 1
+
+    # build filesec
+    # print(pres_master_json)
+    filesec = mm.FileSec()
+    if pres_master_json != None:
+        # print("found presmasterdir")
+        filegrp = mm.FileGrp(ID="rep{}".format(pm_rep_no),
+                    ADMID="rep{}-amd".format(pm_rep_no))
+        _parse_json_for_filegrp(filegrp, pm_rep_no, pres_master_json, input_dir)
+        filesec.append(filegrp)
+    if modified_master_json != None:
+        filegrp = mm.FileGrp(ID="rep{}".format(mm_rep_no),
+                    ADMID="rep{}-amd".format(mm_rep_no))
+        _parse_json_for_filegrp(filegrp, mm_rep_no, modified_master_json, input_dir)
+        filesec.append(filegrp)
+    if access_derivative_json != None:
+        filegrp = mm.FileGrp(ID="rep{}".format(ad_rep_no),
+                    ADMID="rep{}-amd".format(ad_rep_no))
+        _parse_json_for_filegrp(filegrp, ad_rep_no, modified_master_json, input_dir)
+        filesec.append(filegrp)
+
+
+    mets.append(filesec)
+
+
+    # build structmaps
+    structmap = mm.StructMap(ID="rep{}-1".format(rep_no), TYPE="PHYSICAL")
+    if pres_master_json != None:
+        div1 = mm.Div(LABEL='Preservation Master')
+        structmap.append(div1)
+        mets.append(structmap)
+        _parse_json_for_structmap(div1, pm_rep_no, pres_master_json)
+    if modified_master_json != None:
+        div1 = mm.Div(LABEL='Modified Master')
+        structmap.append(div1)
+        mets.append(structmap)
+        _parse_json_for_structmap(div1, mm_rep_no, modified_master_json)
+    if access_derivative_json != None:
+        div1 = mm.Div(LABEL='Access Derivative')
+        structmap.append(div1)
+        mets.append(structmap)
+        _parse_json_for_structmap(div1, ad_rep_no, access_derivative_json)
 
     return mets
--- tests/metsdnx_test.py
+++ tests/metsdnx_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from nose.tools import *
@@ -5,6 +6,8 @@ from lxml import etree as ET
 
 from mets_dnx import factory as mdf
 
+
+CURRENT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 def test_mets_dnx():
     """Test basic construction of METS DNX"""
@@ -226,3 +229,77 @@ def test_structmap_for_single_file_mets_has_upper_case_type():
     assert(structmap.attrib["TYPE"] == "PHYSICAL")
     print(structmap.attrib["TYPE"])
     assert(structmap.attrib["TYPE"] != "Physical")
+
+
+def test_mets_dnx_with_for_single_rep():
+    """
+    """
+    ie_dc_dict = {"dc:title": "test title"}
+    mets = mdf.build_mets(
+        ie_dmd_dict=ie_dc_dict,
+        pres_master_dir=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'data',
+            'test_batch_1',
+            'pm'),
+        input_dir=os.path.join(os.path.dirname(
+            os.path.realpath(__file__)),
+            'data',
+            'test_batch_1',
+            'pm'),
+        generalIECharacteristics=[{
+            'submissionReason': 'bornDigitalContent',
+            'IEEntityType': 'periodicIE'}],
+        digital_original=True
+        )
+    digital_original_el = mets.xpath('.//key[@id="DigitalOriginal"]')[0]
+    assert(digital_original_el.text == "true")
+
+
+def test_mets_dnx_with_json():
+    """For testing new function for building SIP with JSON documents
+    describing the structure and metadata of files."""
+    ie_dc_dict = {"dc:title": "test title"}
+    pm_json = """[{"name": "%s",
+                "type": "directory",
+                "children":
+                    [{"name": "path",
+                      "type": "directory",
+                      "children":
+                        [{"name": "to",
+                          "type": "directory",
+                          "children":
+                            [{"name": "files",
+                              "type": "directory",
+                              "children":
+                                [{"name": "img1.jpg",
+                                  "type": "file",
+                                  "MD5": "aff64bf1391ac627edb3234a422f9a77",
+                                  "fileCreationDate": "1st of January, 1601",
+                                  "fileModificationDate": "1st of January, 1601",
+                                  "label": "Image One",
+                                  "note": "This is a note for image 1"},
+                                 {"name": "img2.jpg",
+                                  "type": "file",
+                                  "MD5": "9d09f20ab8e37e5d32cdd1508b49f0a9",
+                                  "fileCreationDate": "1st of January, 1601",
+                                  "fileModificationDate": "1st of January, 1601",
+                                  "label": "Image Two",
+                                  "note": "This is a note for image 2"
+                                  }
+                                ]
+                             }
+                            ]
+                          }
+                        ]
+                     }
+                    ]
+                }]""" % (os.path.join(CURRENT_DIR, "data", "test_batch_2"))
+    # print(os.path.join(CURRENT_DIR, "data", "test_batch_2"))
+    # print(pm_json)
+    mets = mdf.build_sip_from_json(
+        ie_dmd_dict=ie_dc_dict,
+        pres_master_json = pm_json,
+        input_dir=os.path.join(CURRENT_DIR, 'data', 'test_batch_2'),
+        digital_original=True)
+    print(ET.tounicode(mets, pretty_print=True))
