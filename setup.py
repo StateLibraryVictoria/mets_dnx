ea44bcd22db8aa1d3c63dd33a4be78afe1346afb
Sean Mosely <sean.mosely@dia.govt.nz>
2018-01-17 14:33:43 -0800

    modified lxml requirement in setup.py from '==' to '>='

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
 	'author_email':'sean.mosely@gmail.com',
 	'packages':['mets_dnx',],
 	'description':'Python library for building Rosetta DNX/METS XML documents',
-	'install_requires':['lxml==3.6.4', 'pymets','pydc', 'pydnx'],
+	'install_requires':['lxml>=3.6.4', 'pymets','pydc', 'pydnx'],
 	'download_url': 'https://github.com/NLNZDigitalPreservation/mets_dnx/archive/v'+VERSION+'.tar.gz',
 	'license': 'MIT',
 	}
